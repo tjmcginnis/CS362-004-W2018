aebf937c70d393b04353de1eeb70431047b170b4
Tyler McGinnis <tylermcginnis@gmail.com>
2018-02-03 23:39:46 -0800

    Removed commented out copy of fullDeckCount

--- projects/mcginnit/dominion/unittest4.c
+++ projects/mcginnit/dominion/unittest4.c
@@ -22,28 +22,6 @@
  *
  */
 
-// int fullDeckCount(int player, int card, struct gameState *state) {
-//   int i;
-//   int count = 0;
-
-//   for (i = 0; i < state->deckCount[player]; i++)
-//     {
-//       if (state->deck[player][i] == card) count++;
-//     }
-
-//   for (i = 0; i < state->handCount[player]; i++)
-//     {
-//       if (state->hand[player][i] == card) count++;
-//     }
-
-//   for (i = 0; i < state->discardCount[player]; i++)
-//     {
-//       if (state->discard[player][i] == card) count++;
-//     }
-
-//   return count;
-// }
-
 void testFullDeckCount () {
     struct gameState G;
     int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse,
